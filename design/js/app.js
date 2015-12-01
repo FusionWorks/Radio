e84ac473c762c60ed8077661e946a5fb0d0434d0
Alexei Nasonov <bobbilly@MacBookPro.local>
2015-12-01 17:02:44 +0200

    my First Commit

---
+++ design/js/app.js
@@ -0,0 +1,6 @@
+$(document).foundation();
+$('.content-wrapper').backstretch([
+      "http://dl.dropbox.com/u/515046/www/outside.jpg"
+    , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
+    , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
+  ], {duration: 3000, fade: 750});
