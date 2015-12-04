1cd0ff2cfcb28999535e252da9e5376119b4561f
Alexei Nasonov <bobbilly@MacBookPro.local>
2015-12-04 14:33:09 +0200

    Whole new Commit

--- design/js/app.js
+++ design/js/app.js
@@ -1,6 +1,25 @@
+$( document ).ready(function() {
+
 $(document).foundation();
-$('.content-wrapper').backstretch([
-      "http://dl.dropbox.com/u/515046/www/outside.jpg"
-    , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
-    , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
-  ], {duration: 3000, fade: 750});
+  $('.off-canvas-content').backstretch([
+        "http://dl.dropbox.com/u/515046/www/outside.jpg"
+      , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
+      , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
+    ], {duration: 3000, fade: 750});
+
+  var volume = $('.volume-btn');
+  volume.on("click", function() {
+    $(this).addClass('active');
+  });
+  $('body').click(function(e) {
+      if ( !(
+              $.contains(volume[0], e.target) ||
+              volume.is(e.target)                 ||
+              $('.js-callback-control').is(e.target)
+          )
+      ) {
+         volume.removeClass('active');
+      }
+  });
+
+});
